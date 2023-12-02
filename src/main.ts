aaeb8e1bddb56417eac3a4347b2f67be5304b25a
Hyeonseok Jang <jang.hyeonseok@linkbaljang.local>
2023-12-02 22:12:28 +0900

    FIX cors setting

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,15 @@ import { config } from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `http://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}`,
+    origin: `http://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}/*`,
     credentials: true,
     methods: ['POST', 'PUT', 'GET', 'DELETE'],
     allowedHeaders: [
       'Access-Control-Allow-Origin',
       'Origin',
-      'X-Api-Key',
       'X-Requested-With',
       'Content-Type',
       'Accept',
-      'Authorization',
     ],
   });
 
